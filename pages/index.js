fa1f6307bf8c1f578f8736920f7dc586c30dbddb
Jamieson Roberts <jamieson.roberts@saxxunderwear.com>
2023-02-20 13:19:25 -0500

    Legacy content and footer social links

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,10 @@ export default function Home() {
     return (
         <>
             <Head>
-                <title>Project Play | Game.Play.Give | Thank You</title>
+                <title>
+                    Project Play | Game.Play.Give | Thank you for 6 amazing
+                    years
+                </title>
                 <meta name='description' content='Project Play' />
             </Head>
             <Header className={'container'} />
